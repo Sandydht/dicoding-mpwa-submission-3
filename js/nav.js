4eeb23becc4965a2ecd32034150904faf41aa07a
Sandydht <48269439+Sandydht@users.noreply.github.com>
2020-05-27 01:28:28 +0700

    first commit

---
+++ js/nav.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const elems = document.querySelectorAll(".sidenav");
+    M.Sidenav.init(elems);
+
+    function loadNav() {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                if (this.status !== 200) return;
+
+                document.querySelectorAll(".topnav, .sidenav").forEach(function (elm) {
+                    elm.innerHTML = xhttp.responseText;
+                });
+
+                document.querySelectorAll(".topnav a, .sidenav a").forEach(function (elm) {
+                    elm.addEventListener("click", function (event) {
+                        const sidenav = document.querySelector(".sidenav");
+                        M.Sidenav.getInstance(sidenav).close();
+
+                        page = event.target.getAttribute("href").substr(1);
+                        loadPage(page);
+                    })
+                });
+            }
+        }
+        xhttp.open("GET", "nav.html", true);
+        xhttp.send();
+    }
+
+    let page = window.location.hash.substr(1);
+    if (page === "") page = "home";
+    function loadPage(page) {
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                let content = document.getElementById("body-content");
+                if (this.status == 200) {
+                    content.innerHTML = xhttp.responseText;
+                } else if (this.status == 404) {
+                    content.innerHTML = "<p>Halaman tidak ditemukan</p>";
+                } else {
+                    content.innerHTML = "<p>Halaman tidak dapat diakses</p>";
+                }
+            }
+        }
+        xhttp.open("GET", `pages/${page}.html`, true);
+        xhttp.send();
+    }
+
+    loadNav();
+    loadPage(page);
+});
